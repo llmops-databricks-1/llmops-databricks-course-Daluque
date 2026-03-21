2a23d8f3eaa97cd2e88d3f6a9f82b6a4dd14cc45
DL <luque.david.andres@gmail.com>
2026-03-20 22:30:26 -0400

    Deployment fix for Laboratory 1

--- typings/__builtins__.pyi
+++ typings/__builtins__.pyi
@@ -1,5 +1,5 @@
 
-from databricks.sdk.runtime import *
+from databricks.sdk.runtime import dbutils
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import udf as U
 from pyspark.sql.context import SQLContext
@@ -12,7 +12,7 @@ sql = sqlContext.sql
 table = sqlContext.table
 getArgument = dbutils.widgets.getArgument
 
-def displayHTML(html): ...
+def displayHTML(html: str) -> None: ...
 
-def display(input=None, *args, **kwargs): ...
+def display(input=None, *args, **kwargs) -> None: ...
 
